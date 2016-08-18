3fc2cf2b0f9c158f9b9eb605c1e48989aa00e02f
William Lee <pseudonym.wlee@gmail.com>
2016-08-18 20:30:05 +0100

    Changed results layout to be more gmail like.

--- app/src/main/java/com/example/will/sfclippy/ResultsActivity.java
+++ app/src/main/java/com/example/will/sfclippy/ResultsActivity.java
@@ -1,11 +1,14 @@
 package com.example.will.sfclippy;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
@@ -16,8 +19,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.google.android.gms.vision.text.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -27,43 +34,46 @@ import java.util.List;
 /**
  * Created by will on 13/08/2016.
  */
-public class ResultsActivity extends Activity {
+public class ResultsActivity extends AppCompatActivity {
     public static class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ViewHolder> {
         private List<DataProvider.BattleResult> results;
-        private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM");
         private String p1Id;
         private String p2Id;
-        private Drawable winImg;
+        private Drawable p1Img;
+        private Drawable p2Img;
 
         /**
          * Elements of the view.
          */
         public static class ViewHolder extends RecyclerView.ViewHolder {
+            public ImageView winnerImg;
             public TextView dateView;
-            public TextView p1Player;
-            public TextView p2Player;
+            public TextView resultPairing;
 
             public ViewHolder( View container ) {
                 super(container);
-                dateView = (TextView) container.findViewById( R.id.textResultDate );
-                p1Player = (TextView) container.findViewById( R.id.textResultP1 );
-                p2Player = (TextView) container.findViewById( R.id.textResultP2 );
+                winnerImg = (ImageView) container.findViewById( R.id.textResultImg );
+                dateView = (TextView) container.findViewById( R.id.textResultDateLabel );
+                resultPairing = (TextView) container.findViewById( R.id.textResultPairing );
             }
         }
 
         public ResultsAdapter( List<DataProvider.BattleResult> results,
                                String p1Id,
                                String p2Id,
-                               Drawable winImg ) {
+                               Drawable p1Img,
+                               Drawable p2Img ) {
             this.results = results;
             this.p1Id = p1Id;
             this.p2Id = p2Id;
-            this.winImg = winImg;
+            this.p1Img = p1Img;
+            this.p2Img = p2Img;
         }
 
         @Override
         public ResultsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType ) {
-            LinearLayout entry = (LinearLayout) LayoutInflater.from( parent.getContext() )
+            View entry = LayoutInflater.from( parent.getContext() )
                     .inflate( R.layout.layout_result, parent, false );
             ViewHolder vh = new ViewHolder( entry );
             return vh;
@@ -78,28 +88,16 @@ public class ResultsActivity extends Activity {
         public void onBindViewHolder( ViewHolder holder, int position ) {
             DataProvider.BattleResult result = results.get(position);
 
-            StyleSpan boldSpan = new StyleSpan(Typeface.BOLD );
-
             CharSequence p1String = result.characterFor( p1Id );
-            if ( result.winner( p1Id) ) {
-                SpannableStringBuilder str = new SpannableStringBuilder( p1String );
-                str.setSpan( boldSpan, 0, p1String.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE );
-                /* str.setSpan( new ImageSpan( winImg, "src", DynamicDrawableSpan.ALIGN_BOTTOM ),
-                        0, 1,
-                        Spannable.SPAN_INCLUSIVE_INCLUSIVE ); */
-                p1String = str;
-            }
-
             CharSequence p2String = result.characterFor( p2Id );
-            if ( result.winner( p2Id) ) {
-                SpannableString str = new SpannableString(p2String);
-                str.setSpan( boldSpan, 0, p2String.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE );
-                p2String = str;
-            }
 
+            if ( result.winner(p1Id) ) {
+                holder.winnerImg.setImageDrawable( p1Img );
+            } else {
+                holder.winnerImg.setImageDrawable( p2Img );
+            }
+            holder.resultPairing.setText( p1String + " vs " + p2String );
             holder.dateView.setText( dateFormat.format( result.getDate()) );
-            holder.p1Player.setText( p1String );
-            holder.p2Player.setText( p2String );
         }
     }
 
@@ -107,6 +105,10 @@ public class ResultsActivity extends Activity {
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbarMain);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         final RecyclerView listView = (RecyclerView) findViewById( R.id.resultsList );
         listView.setHasFixedSize(true);
@@ -119,15 +121,8 @@ public class ResultsActivity extends Activity {
         String player1Id = provider.getPlayer1Id();
         String player2Id = provider.getPlayer2Id();
 
-        TextView p1Label = (TextView) findViewById(R.id.textResultLabelP1);
-        p1Label.setText( provider.getPlayer1Name() );
-
-        TextView p2Label = (TextView) findViewById(R.id.textResultLabelP2);
-        p2Label.setText( provider.getPlayer2Name() );
-
         // fetch results to display
-        List<DataProvider.BattleResult> results = AppSingleton.getInstance().getDataProvider()
-                .getCurrentPlayerResults();
+        List<DataProvider.BattleResult> results = provider.getCurrentPlayerResults();
         Collections.sort(results, new Comparator<DataProvider.BattleResult>() {
             @Override
             public int compare(DataProvider.BattleResult lhs, DataProvider.BattleResult rhs) {
@@ -140,9 +135,15 @@ public class ResultsActivity extends Activity {
                 getTheme() );
         winImg.setBounds(0, 0, winImg.getIntrinsicWidth(), winImg.getIntrinsicHeight() );
 
+        TextDrawable p1Img = TextDrawable.builder()
+                .buildRound( provider.getPlayer1Name().substring(0, 1), Color.RED );
+        TextDrawable p2Img = TextDrawable.builder()
+                .buildRound( provider.getPlayer2Name().substring(0, 1), Color.BLUE );
+
         // create adapter
         Log.d( getLocalClassName(), "Creating adapter for " + results.size());
-        RecyclerView.Adapter mAdapter = new ResultsAdapter( results, player1Id, player2Id, winImg );
+        RecyclerView.Adapter mAdapter = new ResultsAdapter( results, player1Id, player2Id,
+                p1Img, p2Img );
         listView.setAdapter(mAdapter);
     }
 }
